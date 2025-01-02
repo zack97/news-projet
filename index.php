0f78c101185ab1fbcb0dd45bf81864b05ed9986c
zack10 <tshibzack@gmail.com>
2025-01-02 13:52:36 +0100

    updating add

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
 session_start(); 
 
 require_once './View/includes/body.php'; 
-
+require_once './View/includes/ads.php';
 
 
 generatehead('./View/assets/css/main.css');
 generateHeader('./View/media/news.jpg', './View/controllers/log_in.php', './View/controllers/logout.php', './favorites_list.php');
 generatenav('./View/controllers/recherche.php');
+publicite();
 generatearticle('./Model/articles.json');
 generatefooter();
 generateboottraap();
